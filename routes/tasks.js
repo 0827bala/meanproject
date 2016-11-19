c615d8e4e05791411d07eb2e405e01903e5d9eae
0827bala <abalasubramani827@gmail.com>
2016-11-19 14:21:53 +0530

    tasks js file edited

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var mongojs = require('mongojs');
+var db = mongojs('mongodb://anandprvn:anandprvn@ds157677.mlab.com:57677/developmentdb', ['tasks']);
 
 router.get('/tasks', function(req, res, next) {
-    // res.render('index.html');
-    res.send('Tasks Page');
+    db.tasks.find(function(err, tasks) {
+        if (err) {
+            res.send(err);
+        }
+        res.json(tasks);
+    });
+});
+
+// // Get Single Task
+router.get('/task/:id', function(req, res, next) {
+    db.tasks.findOne({ _id: mongojs.ObjectId(req.params.id) }, function(err, task) {
+        if (err) {
+            res.send(err);
+        }
+        res.json(task);
+    });
 });
 
 module.exports = router;
